5cafa9eb8ebbda3fb85cd9f264ea1732180e973d
Gordon Murray <gordon@gordonmurray.com>
2025-05-04 12:54:15 +0100

    Added Cors settings for a front end

--- producer/app.py
+++ producer/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import boto3, uuid, os
 import aio_pika
 
@@ -15,6 +16,15 @@ s3 = boto3.client(
     endpoint_url=R2_ENDPOINT,
     aws_access_key_id=R2_KEY,
     aws_secret_access_key=R2_SECRET,
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.post("/upload")
